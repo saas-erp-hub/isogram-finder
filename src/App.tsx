de7a26c73b64d8e30e5e89450d17631f2ef875ab
ByteBuilder <bytebuilder@users.noreply.github.com>
2025-07-24 18:22:51 +0200

    feat: Integrate Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import IsogramFinder from './IsogramFinder';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App" role="main">
       <IsogramFinder />
+      <Analytics />
     </div>
   );
 }
